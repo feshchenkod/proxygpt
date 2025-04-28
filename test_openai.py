0c0aba5a0ce199ca3f88b84174ad6862dd6f7ce6
_den <_den@outlook.com>
2025-04-28 22:59:53 +1000

    use async

--- test_openai.py
+++ test_openai.py
@@ -1,19 +1,22 @@
+import asyncio
 from dotenv import load_dotenv
 import os
-from openai import OpenAI
+from openai import AsyncOpenAI
 
 load_dotenv()
 OPENAI_API_KEY = os.getenv("AI_TOKEN")
 
-if __name__ == '__main__':
-    client = OpenAI(
+async def main() -> None:
+    client = AsyncOpenAI(
         api_key=OPENAI_API_KEY,
     )
 
-    response = client.responses.create(
+    response = await client.responses.create(
         model="gpt-3.5-turbo",
         instructions="You are a coding assistant that talks like a pirate.",
         input="How do I check if a Python object is an instance of a class?",
     )
-
     print(response.output_text)
+
+if __name__ == '__main__':
+    asyncio.run(main())
